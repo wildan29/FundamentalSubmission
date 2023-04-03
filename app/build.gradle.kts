ecbe256a1a3f9f97794ccda9124fb0eba56bec96
wildan <wildanal577@gmail.com>
2023-04-03 22:05:45 +0700

    First Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.dicoding.submissionbeginnerandroid"
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.dicoding.submissionbeginnerandroid"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // default implemenation
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // splash screen
+    implementation("androidx.core:core-splashscreen:1.0.0")
+
+    // test
+    testImplementation("junit:junit:4.13.2")
+
+    // android test
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Circle Image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    // glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+}
+
+tasks.register("copyAPKDebug",Copy::class){
+    dependsOn("test")
+    val soureDir = layout.buildDirectory.dir("outputs/apk/debug/app-debug.apk")
+    val destDir = "$rootDir/apk"
+    from(soureDir)
+    into(destDir)
+    rename("app-debug.apk","BeginnerAndroidDebug.apk")
+
+    // Untuk cek apakah aplikasi ada virus atau tidak, bisa diliath dari MD5 yang sudah d generate
+    doLast{
+        val filePath = File(destDir,"BeginnerAndroidDebug.apk")
+        ant.withGroovyBuilder {
+            "checksum"("file" to filePath.path)
+        }
+    }
+}
+
+tasks.whenTaskAdded {
+    if(this.name == "assembleDebug"){
+        this.finalizedBy("copyAPKDebug")
+    }
+}
